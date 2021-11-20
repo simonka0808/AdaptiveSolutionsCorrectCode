c6c1fc5249b1d73e81888cc0f9564d0a01d903e6
rfindlay2 <56404021+rfindlay2@users.noreply.github.com>
2021-11-20 18:03:13 +0000

    console.logs

--- Backend Prototype/server.js
+++ Backend Prototype/server.js
@@ -148,15 +148,14 @@ app.get("/edit_event", function (req, res) {
   }
 
   var id = req.query.event;
-
-
-
+  console.log("working 1");
   db.collection("events").find({ session_id: id }).toArray(function (err, result) {
     if (err) throw err;
-
+    console.log("working 2");
     res.render("pages/edit_event", {
       event: result,
     });
+
   });
 });
 
