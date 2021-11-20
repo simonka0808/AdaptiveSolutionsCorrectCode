9f8e9d7b1a9f0f8ffc312170c1f0f3981fb9b55b
rfindlay2 <56404021+rfindlay2@users.noreply.github.com>
2021-11-20 18:19:13 +0000

    session name

--- Backend Prototype/server.js
+++ Backend Prototype/server.js
@@ -149,7 +149,7 @@ app.get("/edit_event", function (req, res) {
 
   var id = req.query.event;
   console.log("working 1" + id);
-  db.collection("events").findOne({ session_id: id },function (err, result) {
+  db.collection("events").findOne({ session_name: id },function (err, result) {
     if (err) throw err;
     console.log(result);
     res.render("pages/edit_event", {
