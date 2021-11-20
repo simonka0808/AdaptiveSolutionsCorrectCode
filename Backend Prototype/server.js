899b7ab39626fb871bd36cea024a05bf108d3826
Your name <1712991@rgu.ac.uk>
2021-11-20 18:55:27 +0000

    req

--- Backend Prototype/server.js
+++ Backend Prototype/server.js
@@ -357,7 +357,7 @@ app.post("/editevent", function (req, res) {
   //we create the data string from the form components that have been passed in
 
   db.collection("events").findOne(
-    {session_id: req.session.currentuser },
+    {session_name: req.query.event },
     function (err, current_entry) {
       if (err) throw err;
       console.log(current_entry);
